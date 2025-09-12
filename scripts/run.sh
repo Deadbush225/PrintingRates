8f76030484f432fda90eca17f999260b68be36b2
Deadbush225 <eliazar225ninso@gmail.com>
2025-09-13 00:30:56 +0800

    (feat) install boost

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 git add -A
-git commit -m "(feat) cross compatibility"
+git commit -m "(feat) install boost"
 git tag -f v0.0.7
 # git push origin main
 git push origin main --tags -f
