016f7ba5b366a0da83c734c3d03e9dc8b4f77b0b
Deadbush225 <eliazar225ninso@gmail.com>
2025-09-13 15:15:52 +0800

    (fix) windeployqt doesn't place the dll in the install/bin folder

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 git add -A
-git commit -m "(feat) install boost"
+git commit -m "(fix) windeployqt doesn't place the dll in the install/bin folder"
 git tag -f v0.0.7
 # git push origin main
 git push origin main --tags -f
