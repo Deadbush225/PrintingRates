619ad4c2dea8b98d0b67354ddf48756b474361d1
Deadbush225 <eliazar225ninso@gmail.com>
2025-09-13 16:02:33 +0800

    (fix) update windeployqt verification

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 git add -A
-git commit -m "(fix) windeployqt doesn't place the dll in the install/bin folder"
+git commit -m "(fix) update windeployqt verification"
 git tag -f v0.0.7
 # git push origin main
 git push origin main --tags -f
